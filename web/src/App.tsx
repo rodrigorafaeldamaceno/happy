80af7902c80599fea313016fdb7f36d2ca6c3463
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2020-10-12 16:00:56 -0300

    created landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
+
+import './styles/global.css';
+import './styles/pages/page-landing.css';
+
+import logoIMg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoIMg} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Medianeira</strong>
+          <span>Paraná</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={25} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
