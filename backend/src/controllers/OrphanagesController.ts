2f1252c97e25f3911cdf5718f5783cbfc046414c
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2020-10-13 14:54:09 -0300

    created views

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,8 @@ import { request, Request, Response } from  'express';
 import { getRepository } from "typeorm";
 import Orphanage from '../models/Orphanage';
 
+import orphanageView from '../views/orphanagesView'
+
 export default {
   async create(req: Request, res: Response){
     const { name, latitude, longitude, about, opening_hours, open_on_weekends, instructions } = req.body;
@@ -13,7 +15,6 @@ export default {
     const images = requestImages.map((image)=>{
       return { path: image.filename }
     });
-    console.log(images);
 
     const orphanages = orphanagesRepository.create({
       name,
@@ -33,19 +34,21 @@ export default {
   async index(req: any, res:any){
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({relations:['images']});
 
-    return res.json(orphanages);
+    return res.json(orphanageView.renderMany(orphanages));
   },
   async show(req: any, res:any){
     const { id } = req.params;
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id).catch((error) => {
-      return res.status().json(error)
-    });
+    const orphanage = await orphanagesRepository.findOneOrFail(id, { relations:['images'] })
+      .catch((error) => {
+        return res.status().json(error)
+      }
+    );
 
-    return res.json(orphanage);
+    return res.json(orphanageView.render(orphanage));
   },
 }
